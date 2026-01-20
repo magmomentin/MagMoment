0ae310783b631eb37309c885c49445cb5eefb9ef
magmomentin <magmoment.in@gmail.com>
2026-01-20 23:54:10 +0530

    Add files via upload

--- ui/uiManager.js
+++ ui/uiManager.js
@@ -1,1 +1,6 @@
-
+export class UIManager{
+ constructor(){this.hint=document.getElementById("hint");}
+ loading(){this.hint.innerText="Loading AR...";}
+ found(){this.hint.innerText="Hold steady";}
+ lost(){this.hint.innerText="Point camera at the frame";}
+}
