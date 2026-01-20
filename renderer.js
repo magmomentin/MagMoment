b21ee65db1ce8ecfe68e0f73aba57c9536207b89
magmomentin <magmoment.in@gmail.com>
2026-01-21 00:21:16 +0530

    Delete renderer.js

--- renderer.js
+++
@@ -1,7 +0,0 @@
-export function setupRenderer(canvas, video){
- const ctx = canvas.getContext("2d");
- function resize(){ if(!video.videoWidth) return;
-  canvas.width=video.videoWidth; canvas.height=video.videoHeight;}
- video.addEventListener("loadedmetadata",resize); window.addEventListener("resize",resize);
- return ctx;
-}
